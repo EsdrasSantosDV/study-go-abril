133071bea4f4f8a56d36de5897f411f94793f38f
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-04-08 19:54:38 -0300

    feat:adding values

---
+++ 2-values.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//pra printar uma string, usa o fmt.Println
+
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+
+	fmt.Println("", 129+124)
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	//operadores lógicos
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	fmt.Println("1+1 =", 9%2)
+}
