6ca3735f73e973f3e4ee5e8ff8fa744366bc0846
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-04-08 20:46:04 -0300

    feat:if

---
+++ 6-if-else.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//IF E IGUAL IF DE C
+	if 100%2 == 0 {
+		fmt.Println("esdras")
+	} else {
+		fmt.Println("esdras 2")
+	}
+	//NÃO PODE FICAR ESPAÇO ENTRE O IF E O PARENTESES
+
+}
