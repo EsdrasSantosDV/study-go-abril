1eebd8c84df756abd8b40b3cdbea5550b259aab4
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2024-04-11 20:11:21 -0300

    feat:slices

---
+++ 9-slices.go
@@ -0,0 +1,63 @@
+package main
+
+//PRA USAR O SLICES PRECISA IMPORTAR O PACOTE SLICES
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+
+	//Uma fatia não inicializada é igual a nula e tem comprimento 0.
+	var s []string
+	fmt.Println("não inicializada:", s, s == nil, len(s) == 0)
+
+	//UMA FATIA as fatias são digitadas apenas pelos elementos que contêm (não pelo número de elementos).
+	s = make([]string, 4)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+
+	//PODEMOS ADICIONAR ELEMENTOS A UMA FATIA USANDO A FUNÇÃO append
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	s = append(s, "g", "h", "i")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	t2 := []string{"g", "h", "i"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+}
